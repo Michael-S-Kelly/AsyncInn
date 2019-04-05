7d5daee162033d63a62e6c6ef725a4d9d35d916f
Michael Kelly <mike.s.kelly@icloud.com>
2019-04-05 00:01:22 -0700

    Fixed Db connection/configuration

--- AsyncInn/Data/AsyncInnDbContext.cs
+++ AsyncInn/Data/AsyncInnDbContext.cs
@@ -1,17 +1,32 @@
-﻿using System;
+﻿using AsyncInn.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-//using AsyncInn.Models;
-using Microsoft.EntityFrameworkCore;
 
 namespace AsyncInn.Data
 {
     public class AsyncInnDbContext : DbContext
     {
+        public DbSet<Amenities> Amenities { get; set; }
+        public DbSet<RoomAmenities> RoomAmenities { get; set; }
+        public DbSet<Room> Room { get; set; }
+        public DbSet<HotelRoom> HotelRoom { get; set; }
+        public DbSet<Hotel> Hotel { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<RoomAmenities>().HasKey(ra => new { ra.RoomID, ra.AmenitiesID });
+            modelBuilder.Entity<HotelRoom>().HasKey(hr => new { hr.HotelID, hr.RoomID });
+
+        }
+
         public AsyncInnDbContext(DbContextOptions<AsyncInnDbContext> options) : base(options)
         {
 
         }
+
+
     }
 }
