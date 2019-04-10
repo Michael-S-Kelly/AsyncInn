22f07062c293e489c58ea9bfd3726fb217b9e619
Michael Kelly <mike.s.kelly@icloud.com>
2019-04-09 23:34:02 -0700

    Added interfaces and HotelServices

---
+++ AsyncInn/Models/Interfaces/IHotelsManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Models.Interfaces
+{
+    public interface IHotelsManager
+    {
+        Task CreateHotel(Hotel hotel);
+
+        void UpdateHotel(int id, Hotel hotel);
+
+        void DeleteHotel(int id, Hotel hotel);
+
+        Task<Hotel> GetHotel(int id);
+
+        Task<List<Hotel>> GetHotels();
+
+        Task<List<HotelRoom>> GetHotelRooms(int id);
+        Task<HotelRoom> GetHotelRoom(int roomNumber);
+        Task AssignRoom(HotelRoom hotelRoom);
+        Task UpdateRoomAssignment(HotelRoom hotelRoom);
+        Task RemoveRoomAssignment(int roomNumber);
+
+        
+
+        bool HotelRoomExists(int roomNumber);
+
+        bool HotelExists(int id);
+    }
+}
---
+++ AsyncInn/Models/Services/HotelServices.cs
@@ -0,0 +1,90 @@
+﻿using AsyncInn.Data;
+using AsyncInn.Models.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace AsyncInn.Models.Services
+{
+    public class HotelServices : IHotelsManager
+    {
+        private AsyncInnDbContext _context;
+
+        public HotelServices(AsyncInnDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AssignRoom(HotelRoom hotelRoom)
+        {
+            _context.Add(hotelRoom);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task CreateHotel(Hotel hotel)
+        {
+            _context.Add(hotel);
+            await _context.SaveChangesAsync();
+        }
+
+        public void DeleteHotel(int id, Hotel hotel)
+        {
+            _context.Remove(hotel);
+            _context.SaveChanges();
+        }
+
+        public async Task<Hotel> GetHotel(int id)
+        {
+            Hotel hotel = await _context.Hotel
+                .Include(hr => hr.HotelRoom)
+                .ThenInclude(r => r.Room)
+                .FirstOrDefaultAsync(i => i.ID == id);
+            return hotel;
+        }
+
+        public async Task<HotelRoom> GetHotelRoom(int roomNumber)
+        {
+            return await _context.HotelRoom.FirstOrDefaultAsync(rn => rn.RoomNumber == roomNumber);
+        }
+
+        public async Task<List<HotelRoom>> GetHotelRooms(int id)
+        {
+            return await _context.HotelRoom.Where(r => r.RoomID == id).ToListAsync();
+        }
+
+        public async Task<List<Hotel>> GetHotels()
+        {
+            return await _context.Hotel.ToListAsync();
+        }
+
+        public bool HotelRoomExists(int roomNumber)
+        {
+            return _context.HotelRoom.Any(rn => rn.RoomNumber == roomNumber);
+        }
+
+        public bool HotelExists(int id)
+        {
+            return _context.Hotel.Any(r => r.ID == id);
+        }
+
+        public async Task RemoveRoomAssignment(int roomNumber)
+        {
+            var rmNum = await GetHotelRoom(roomNumber);
+            _context.HotelRoom.Remove(rmNum);
+        }
+
+        public async void UpdateHotel(int id, Hotel hotel)
+        {
+            _context.Update(hotel);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateRoomAssignment(HotelRoom hotelRoom)
+        {
+            _context.Update(hotelRoom);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
